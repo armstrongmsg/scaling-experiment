a5e63a9e49aed1e303f0921f522cf7601f030e5f
armstrongmsg <armstrongmsg@gmail.com>
2017-10-05 16:54:43 -0300

    Added support to TeraSort

--- experiment.sh
+++ experiment.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+function run_application()
+{
+	cp $1 "conf/application.cfg"
+	APP_ID="`python scripts/client/client.py conf $MANAGER_IP $MANAGER_PORT $cap $actuator`"
+	APP_ID="`echo $APP_ID | tr -d '"'`"
+			
+	STATUS=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"status\"]"`
+	
+	while [[ $STATUS != "OK" && ($STATUS != "Error") ]]
+	do
+		for instance_id in $INSTANCES_IDS
+		do
+			CPU_DATA_DIR="$EXPERIMENT_CPU_DATA_DIR/$conf/$APP_ID"
+                          
+			mkdir -p $CPU_DATA_DIR
+
+			bash scripts/utils/get_cpu_usage.sh $instance_id 2> error_output.txt >> "$CPU_DATA_DIR/$instance_id"".cpu_data"
+		done
+
+		sleep 1
+		STATUS=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"status\"]"`
+	done
+
+	APPLICATION_TIME=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"time\"]"`
+	APPLICATION_START_TIME=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"start_time\"]"`
+}
+
 REPS=$1
 source conf/experiment.cfg
 
@@ -28,33 +55,25 @@ do
 		
 					cp "treatments/scaling/$conf" "conf/scaling.cfg"
 					
-					#cp "$TREATMENTS_DIR/$conf" "conf/scaling.cfg"
-					cp "treatments/applications/$app.cfg" "conf/application.cfg"
-					APP_ID="`python scripts/client/client.py conf $MANAGER_IP $MANAGER_PORT $cap $actuator`"
-					APP_ID="`echo $APP_ID | tr -d '"'`"
-		
-					STATUS=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"status\"]"`
-		
-					while [[ $STATUS != "OK" && ($STATUS != "Error") ]]
-					do
-						for instance_id in $INSTANCES_IDS
-						do
-							CPU_DATA_DIR="$EXPERIMENT_CPU_DATA_DIR/$conf/$APP_ID"
-		                                  
-							mkdir -p $CPU_DATA_DIR
-		
-							bash scripts/utils/get_cpu_usage.sh $instance_id 2> error_output.txt >> "$CPU_DATA_DIR/$instance_id"".cpu_data"
-						done
-		
-						sleep 1
+					if [ $app = "terasort" ]
+					then
+						TOTAL_APPLICATION_TIME=0		
 						
-						STATUS=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"status\"]"`
-					done
+						run_application "treatments/applications/teragen.cfg"
+						FINAL_APPLICATION_START_TIME=$APPLICATION_START_TIME
+						TOTAL_APPLICATION_TIME=$(( $TOTAL_APPLICATION_TIME + $APPLICATION_TIME ))
 				
-					APPLICATION_TIME=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"time\"]"`
-					APPLICATION_START_TIME=`curl --data "" http://$MANAGER_IP:$MANAGER_PORT/manager/status 2> /dev/null | jq -r ".$APP_ID[\"start_time\"]"`
-			
-					echo "$APP_ID|$conf|$cap|$APPLICATION_TIME|$APPLICATION_START_TIME" >> app_conf.txt
+						run_application "treatments/applications/terasort.cfg"
+						TOTAL_APPLICATION_TIME=$(( $TOTAL_APPLICATION_TIME + $APPLICATION_TIME ))
+						
+						run_application "treatments/applications/teravalidate.cfg"
+						TOTAL_APPLICATION_TIME=$(( $TOTAL_APPLICATION_TIME + $APPLICATION_TIME ))
+						
+						echo "$APP_ID|$conf|$cap|$TOTAL_APPLICATION_TIME|$FINAL_APPLICATION_START_TIME" >> app_conf.txt
+					else
+						run_application "treatments/applications/$app.cfg"
+						echo "$APP_ID|$conf|$cap|$APPLICATION_TIME|$APPLICATION_START_TIME" >> app_conf.txt
+					fi
 				done
 			done
 		done
