83ecb8d0d7edbb85e023769313e89324d68da8d4
armstrongmsg <armstrongmsg@gmail.com>
2017-10-17 16:42:11 -0300

    The resources usage collect script now treats I/O units
    - The values returned by virt-top contain units like "M" and "K". The script now converts those values to bytes and removes the units.

--- scripts/utils/get_cpu_usage.sh
+++ scripts/utils/get_cpu_usage.sh
@@ -6,6 +6,21 @@
 instance_id=$1
 compute_nodes="c4-compute11 c4-compute12 c4-compute22"
 
+function remove_and_convert_tail()
+{
+	if [[ $1 == *"K"* ]]
+	then 
+		result=$(( `echo $1 | tr -d 'K'` * 1024 ))
+	elif [[ $1 == *"M"* ]]
+	then
+		result=$(( `echo $1 | tr -d 'M'` * 1048576 ))
+	else
+		result=$1
+	fi
+
+	echo $result
+}
+
 function get_usage()
 {
 	instance_id=$1
@@ -21,11 +36,14 @@ function get_usage()
 	cpu_usage="`echo $virt_top_output | awk '{print $17}' 2> /dev/null`";
 	cpu_usage="`echo "$cpu_usage*$compute_node_cpus/$instance_cpus" | bc`";
 	
-	read_bytes="`echo $virt_top_output | awk '{print $13}' 2> /dev/null`";
-	written_bytes="`echo $virt_top_output | awk '{print $14}' 2> /dev/null`";
+	read_data="`echo $virt_top_output | awk '{print $13}' 2> /dev/null`";
+	written_data="`echo $virt_top_output | awk '{print $14}' 2> /dev/null`";
 
 	host_cpu_idle="`sar 1 1 | awk 'FNR==4'{'print $9'}`"
 	host_cpu_usage="`echo "100 - $host_cpu_idle" | bc`"
+
+	written_bytes="`remove_and_convert_tail $written_data`"
+	read_bytes="`remove_and_convert_tail $read_data`"
 
 	echo "`date +%s`",$cap,$cpu_usage,$read_bytes,$written_bytes,$host_cpu_usage
 }
