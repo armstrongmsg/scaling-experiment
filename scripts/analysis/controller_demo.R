be3e1c4bf4a5ec82fb3228a218ffefdf2ae91bff
Armstrong Goes <armstrongmsg@gmail.com>
2018-08-30 19:10:11 -0300

    min-max example and libraries
    - Added code for the min-max example which includes a min-max function.
    - Added the used libraries.

--- scripts/analysis/controller_demo.R
+++ scripts/analysis/controller_demo.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+
 theme_white <- function() {
   theme_update(
     plot.title = element_text(size=22),
@@ -11,19 +15,26 @@ theme_white <- function() {
   )
 }
 
+min.max <- function(erro) {
+  base <- 50
+  max <- 100
+  r <- max
+  if (erro > 0) {
+    r <- base
+  }
+  return(r)
+}
+
 theme_set(theme_bw())
 theme_white()
 
 controller_labels <- c(`erro` = "Erro", `prop` = "Proporcional", 
                        `deriv` = "Proporcional-Derivativo", 
-                       `pid` = "PID")
+                       `pid` = "PID", `min.max` = "Min-Max")
 
 factor_p <- 0.2
 factor_d <- 2
 factor_i <- 0.05
-
-#x <- seq(-1, 1, by = 1/16)
-#erro <- cbind(sin(pi*x))
 
 erro <- c(0, 0, 0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.11, 
           0.1, 0.09, 0.08, 0.07, 0.06, 0.05, 0.04, 0.03, 0.02, 0.01, 0, 
@@ -40,6 +51,8 @@ df$deriv <- -(factor_p*df$erro + factor_d*df$diff_err)
 df$diff_err <- -df$diff_err
 df$cum_err <- cumsum(erro)
 df$pid <- -(factor_p*df$erro + factor_d*df$diff_err + factor_i*df$cum_err)
+min.max.data <- sapply(1:length(x), function(x) min.max(df$erro[x]))
+df$min.max <- min.max.data
 
 demo.data <-melt(df, id = c("time"))
 
@@ -81,6 +94,19 @@ ggplot(filter(demo.data, demo.data$variable %in% c("erro", "pid")), aes(x = time
   theme(legend.position="none")
 
 ggsave("pid_demo.png")
+
+#
+# Case 4: Min-Max
+#
+
+ggplot(filter(demo.data, demo.data$variable %in% c("erro", "min.max")), aes(x = time, y = value, colour = variable)) + 
+  geom_line() +
+  xlab("Tempo") +
+  ylab("") +
+  facet_wrap(~variable, labeller = labeller(variable = controller_labels), scales = "free_y") +
+  theme(legend.position="none")
+
+ggsave("minmax_demo.png")
 
 # 
 # All
