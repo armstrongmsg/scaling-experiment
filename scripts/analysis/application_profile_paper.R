50359db42b4ba715d54afbd43f82b08ef793a25a
Armstrong Goes <armstrongmsg@gmail.com>
2019-04-25 15:50:42 -0300

    Added option to create progress plot without base workload
    - And removed some unused code

--- scripts/analysis/application_profile_paper.R
+++ scripts/analysis/application_profile_paper.R
@@ -19,17 +19,6 @@ save_plot <- function(filename) {
   ggsave(paste(PLOT_DIRECTORY, filename, sep = "/"), width = 6, height = 6)
 }
 
-resources_labels <- c(cpu_usage = "CPU (%)", read_bytes = "Leitura (MB)",
-                      written_bytes = "Escrita (MB)", host_cpu_usage = "host_cpu_usage", 
-                      progress = "Progresso")
-
-application_labels <- c(cpu_bound_scripted_experiment = "Aplicação limitada por CPU", 
-                        cpu_bound_scripted_experiment_dist = "CPU",
-                        cpu_bound_scripted = "Aplicação limitada por CPU",
-                        emaas = "EMaaS", kmeans = "K-Means", wordcount_profile = "Wordcount", 
-                        wordcount = "Wordcount", pure_io = "I/O",
-                        pure_io_profile = "I/O")
-
 PLOT_DIRECTORY <- "plots_paper"
 
 theme_set(theme_bw())
@@ -38,11 +27,6 @@ theme_white()
 dir.create(PLOT_DIRECTORY)
 
 progress_profile <- read.csv("progress_profile_aggregated.csv")
-resources_profile <- read.csv("resources_profile_aggregated.csv")
-
-resources_profile$read_bytes <- resources_profile$read_bytes/(1024*1024)
-resources_profile$written_bytes <- resources_profile$written_bytes/(1024*1024)
-resources_profile$cpu_usage <- resources_profile$cpu_usage*100/resources_profile$cap
 
 # Application times
 execution.times <- progress_profile %>% group_by(application, application_id) %>% 
@@ -76,6 +60,16 @@ ggplot(progress_profile_paper, aes(time, progress*100)) +
                      labels=c("CPU", "I/O", "EMaaS")), 
              scales = "free", ncol=1, strip.position="right", 
              labeller = labeller(variable = resources_labels, application = application_labels))
-  
 
 save_plot("progress.png")
+
+ggplot(progress_profile_paper, aes(time, progress*100)) +
+  geom_line(aes(group = application_id)) +
+  xlab("Time (s)") +
+  ylab("Progress (%)") +
+  facet_wrap(~factor(application,levels=c("cpu_bound_scripted_experiment_dist", "pure_io", "emaas"), 
+                     labels=c("CPU", "I/O", "EMaaS")), 
+             scales = "free", ncol=1, strip.position="right", 
+             labeller = labeller(variable = resources_labels, application = application_labels))
+
+save_plot("progress_dist_only.png")
