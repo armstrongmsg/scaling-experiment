ec1f86ba209d3cad956b509791ef34990949aff6
Armstrong Goes <armstrongmsg@gmail.com>
2019-05-02 14:33:50 -0300

    Added plots for more controllers

--- scripts/analysis/controller_demo_paper.R
+++ scripts/analysis/controller_demo_paper.R
@@ -30,7 +30,8 @@ theme_white()
 
 controller_labels <- c(`erro` = "Error", `prop` = "P", 
                        `deriv` = "PD", 
-                       `pid` = "PID", `min.max` = "Min-Max")
+                       `pid` = "PID", `min.max` = "Min-Max",
+                       `d_only` = "D", `i_only` = "I")
 
 factor_p <- 0.5
 factor_d <- 4
@@ -43,20 +44,31 @@ erro <- c(0, 0, 0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.1, 0.
 x <-seq(1, length(erro))
 
 df <- data.frame(time=x,erro=erro)
+
+# Proportional
+prop <- -factor_p*df$erro
+df$prop <- prop
+# Derivative
 diff_err <- sapply(2:length(x), function(x) df$erro[x] - df$erro[x-1])
 diff_err <- c(0, diff_err)
-df <- data.frame(time=x,erro=erro,diff_err=diff_err)
-df$prop <- -factor_p*df$erro
-df$deriv <- -(factor_p*df$erro + factor_d*df$diff_err)
+deriv <- -(factor_p*df$erro + factor_d*diff_err)
+df$diff_err <- diff_err
+df$deriv <- deriv
+# Integral
 df$cum_err <- cumsum(erro)
-df$pid <- -(factor_p*df$erro + factor_d*df$diff_err + factor_i*df$cum_err)
+pid <- -(factor_p*df$erro + factor_d*df$diff_err + factor_i*df$cum_err)
+df$pid <- pid
+# Min-Max
 min.max.data <- sapply(1:length(x), function(x) min.max(df$erro[x]))
 df$min.max <- min.max.data
+# Extra-options
+df$d_only <- -(factor_d*df$diff_err)
+df$i_only <- -(factor_i*df$cum_err)
 
 demo.data <-melt(df, id = c("time"))
 
 # 
-# All
+# All used controllers
 #
 
 ggplot(filter(demo.data, demo.data$variable %in% c("erro", "prop", "deriv", "pid")), aes(x = time, y = value, colour = variable)) + 
@@ -67,3 +79,16 @@ ggplot(filter(demo.data, demo.data$variable %in% c("erro", "prop", "deriv", "pid
   theme(legend.position="none")
 
 ggsave("all_demo.png")
+
+#
+# Extra controllers
+#
+
+ggplot(filter(demo.data, demo.data$variable %in% c("erro", "d_only", "i_only")), aes(x = time, y = value, colour = variable)) + 
+  geom_line() +
+  xlab("Time") +
+  ylab("") +
+  facet_grid(variable ~ ., labeller = labeller(variable = controller_labels)) +
+  theme(legend.position="none")
+
+ggsave("demo_di.png")
