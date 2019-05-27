d6cca9b009f3f7e819497a0b5a45cd2b4e5df2dc
Armstrong Goes <armstrongmsg@gmail.com>
2019-05-27 13:35:11 -0300

    Changed kubejobs client to use multiple init_size values

--- scripts/client/client_kubejobs.py
+++ scripts/client/client_kubejobs.py
@@ -38,13 +38,12 @@ def get_execution_time(broker_ip, broker_port, job_id):
     r = requests.get('http://%s:%s/submissions/%s' % (broker_ip, broker_port, job_id))
     return r.json()[job_id]['execution_time']
 
-def submit_application(experiment_config, conf):
+def submit_application(experiment_config, conf, init_size):
     expected_time = int(experiment_config.get("application", "expected_time"))
     image_name = experiment_config.get("application", "image_name")
     redis_workload = experiment_config.get("application", "redis_workload")
     command = [
         "/factorial/run.py"]
-    init_size = int(experiment_config.get("application", "init_size"))
     
     control_parameters = get_control_parameters(conf)
 
@@ -165,34 +164,39 @@ if __name__ == '__main__':
 
     scaling_confs = experiment_config.get("experiment", "confs").split()
     reps = int(experiment_config.get("experiment", "reps"))
+    init_sizes = experiment_config.get("experiment", "init_size").split()
     
     for rep in xrange(reps):
         print("Rep:%d" % rep)
         
-        for conf in scaling_confs:
-            print ("Conf:%s" % conf)
-    
-            job_id = submit_application(experiment_config, conf)
-            status = get_status(broker_ip, broker_port, job_id)
-    
-            while status != 'ongoing':
-                time.sleep(1)
-                status = get_status(broker_ip, broker_port, job_id)
-            
-            start_time = time.time()
-            
-            while status != 'completed':
-                time.sleep(1)
-                status = get_status(broker_ip, broker_port, job_id)
-                replicas = get_number_of_replicas(k8s_client, job_id)
-                if replicas is not None:
-                    output_file.write("%s,%d,%s,%d,%f\n" % (job_id, rep, conf, replicas,time.time() - start_time))
-                    output_file.flush()
-
-            execution_time = get_execution_time(broker_ip, broker_port, job_id)
+        for i in xrange(len(init_sizes)):
+            init_size = int(init_sizes[i])
+            print ("Init size:%s" % init_size)
         
-            time_output_file.write("%s,%d,%s,%f\n" % (job_id, rep, conf, execution_time))
-            time_output_file.flush()
+            for conf in scaling_confs:
+                print ("Conf:%s" % conf)
+        
+                job_id = submit_application(experiment_config, conf, init_size)
+                status = get_status(broker_ip, broker_port, job_id)
+        
+                while status != 'ongoing':
+                    time.sleep(1)
+                    status = get_status(broker_ip, broker_port, job_id)
+                
+                start_time = time.time()
+                
+                while status != 'completed':
+                    time.sleep(1)
+                    status = get_status(broker_ip, broker_port, job_id)
+                    replicas = get_number_of_replicas(k8s_client, job_id)
+                    if replicas is not None:
+                        output_file.write("%s,%d,%s,%d,%f,%d\n" % (job_id, rep, conf, replicas,time.time() - start_time, init_size))
+                        output_file.flush()
+    
+                execution_time = get_execution_time(broker_ip, broker_port, job_id)
+            
+                time_output_file.write("%s,%d,%s,%f,%d\n" % (job_id, rep, conf, execution_time, init_size))
+                time_output_file.flush()
         
     output_file.close()
     time_output_file.close()
