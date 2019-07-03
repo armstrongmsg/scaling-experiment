dc8ba6c23bbe16def0b11461e341e7c9a1e90c0b
Armstrong Goes <armstrongmsg@gmail.com>
2019-07-02 21:24:46 -0300

    Factorial workload generator now generates random factorial arguments

--- scripts/utils/datagenerator/factorial_workload_generator.py
+++ scripts/utils/datagenerator/factorial_workload_generator.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 
 import sys
+import random
 
-def generate_workload(output_file, factorial_ns, reps):
+def generate_workload(output_file, factorial_ns, reps, random_delta):
     output = open(output_file, "w")
      
     for i in xrange(len(factorial_ns)):
-        n = factorial_ns[i]
         rep = reps[i]
 
         for j in xrange(rep):
+            n = random.randint(factorial_ns[i] - random_delta,
+                               factorial_ns[i] + random_delta)
             output.write("%d\n" % n)
 
     output.close()
@@ -17,14 +19,15 @@ def generate_workload(output_file, factorial_ns, reps):
 if __name__ == '__main__':
  
     output_file = sys.argv[1]
+    random_delta = int(sys.argv[2])
     factorial_ns = []
     reps = []
 
-    for i in xrange(2, len(sys.argv), 2):
+    for i in xrange(3, len(sys.argv), 2):
         factorial_n = int(sys.argv[i])
         rep = int(sys.argv[i + 1])
         factorial_ns.append(factorial_n)
         reps.append(rep)
 
-    generate_workload(output_file, factorial_ns, reps)
+    generate_workload(output_file, factorial_ns, reps, random_delta)
 
